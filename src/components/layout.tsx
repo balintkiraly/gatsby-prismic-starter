16f182dd026c5642267341fa4b82616bf4616be6
balintkiraly <balint.kiraly@protonmail.com>
2020-04-26 22:34:35 +0200

    Condifure Chakra UI

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,6 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { CSSReset, ThemeProvider } from "@chakra-ui/core"
+import { theme } from "../plugins/chakra-ui/theme"
 
 import Header from "./header"
 import "./layout.css"
@@ -24,7 +26,8 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -40,7 +43,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
